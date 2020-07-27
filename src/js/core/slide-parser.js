173dfbc41276c71e2074778494a7e80fdd3ce85f
biati-digital <mannuel@biati.digital>
2020-07-27 12:45:48 -0500

    New slides handler

---
+++ src/js/core/slide-parser.js
@@ -0,0 +1,198 @@
+import { extend, has, each, isNil, isNode, isObject, isNumber } from '../utils/helpers.js';
+
+export default class SlideConfigParser {
+    constructor(el, settings) {
+        this.element = el;
+        this.settings = settings;
+        this.defaults = {
+            href: '',
+            title: '',
+            type: '',
+            description: '',
+            descPosition: '',
+            effect: '',
+            width: '',
+            height: '',
+            node: false,
+            content: false,
+            zoomable: true,
+            draggable: true,
+        };
+    }
+
+    /**
+     * Get source type
+     * gte the source type of a url
+     *
+     * @param {string} url
+     */
+    sourceType(url) {
+        let origin = url;
+        url = url.toLowerCase();
+
+        if (url.match(/\.(jpeg|jpg|jpe|gif|png|apn|webp|svg)$/) !== null) {
+            return 'image';
+        }
+        if (url.match(/(youtube\.com|youtube-nocookie\.com)\/watch\?v=([a-zA-Z0-9\-_]+)/) || url.match(/youtu\.be\/([a-zA-Z0-9\-_]+)/) || url.match(/(youtube\.com|youtube-nocookie\.com)\/embed\/([a-zA-Z0-9\-_]+)/)) {
+            return 'video';
+        }
+        if (url.match(/vimeo\.com\/([0-9]*)/)) {
+            return 'video';
+        }
+        if (url.match(/\.(mp4|ogg|webm|mov)$/) !== null) {
+            return 'video';
+        }
+
+        // Check if inline content
+        if (url.indexOf("#") > -1) {
+            let hash = origin.split('#').pop()
+            if (hash.trim() !== '') {
+                return 'inline'
+            }
+        }
+        // Ajax
+        if (url.includes("gajax=true")) {
+            return 'ajax';
+        }
+
+        return 'external';
+    }
+
+    parseConfig(element, settings) {
+        let data = extend({ descPosition: settings.descPosition }, this.defaults);
+
+        if (isObject(element) && !isNode(element)) {
+            if (!has(element, 'type')) {
+                if (has(element, 'content') && element.content) {
+                    element.type = 'inline';
+                } else if (has(element, 'href')) {
+                    element.type = this.sourceType(element.href);
+                }
+            }
+            let objectData = extend(data, element);
+            this.setSize(objectData, settings);
+
+            return objectData;
+        }
+
+        let url = '';
+        let config = element.getAttribute('data-glightbox')
+        let nodeType = element.nodeName.toLowerCase();
+        if (nodeType === 'a')
+            url = element.href;
+        if (nodeType === 'img')
+            url = element.src;
+
+        data.href = url;
+
+        each(data, (val, key) => {
+            if (has(settings, key) && key !== 'width') {
+                data[key] = settings[key];
+            }
+            const nodeData = element.dataset[key];
+            if (!isNil(nodeData)) {
+                data[key] = this.sanitizeValue(nodeData);
+            }
+        });
+
+        if (data.content) {
+            data.type = 'inline';
+        }
+
+        if (!data.type && url) {
+            data.type = this.sourceType(url);
+        }
+
+        if (!isNil(config)) {
+            let cleanKeys = [];
+            each(data, (v, k) => {
+                cleanKeys.push(';\\s?' + k);
+            })
+            cleanKeys = cleanKeys.join('\\s?:|');
+            if (config.trim() !== '') {
+                each(data, (val, key) => {
+                    const str = config;
+                    const match = '\s?' + key + '\s?:\s?(.*?)(' + cleanKeys + '\s?:|$)';
+                    const regex = new RegExp(match);
+                    const matches = str.match(regex);
+
+                    if (matches && matches.length && matches[1]) {
+                        const value = matches[1].trim().replace(/;\s*$/, '');
+                        data[key] = this.sanitizeValue(value);
+                    }
+                });
+            }
+        } else {
+            if (nodeType == 'a') {
+                let title = element.title
+                if (!isNil(title) && title !== '') data.title = title;
+            }
+            if (nodeType == 'img') {
+                let alt = element.alt
+                if (!isNil(alt) && alt !== '') data.title = alt;
+            }
+            let desc = element.getAttribute('data-description')
+            if (!isNil(desc) && desc !== '') data.description = desc;
+        }
+
+        if (data.description && data.description.substring(0, 1) == '.' && document.querySelector(data.description)) {
+            data.description = document.querySelector(data.description).innerHTML;
+        } else {
+            let nodeDesc = element.querySelector('.glightbox-desc')
+            if (nodeDesc) {
+                data.description = nodeDesc.innerHTML;
+            }
+        }
+
+        this.setSize(data, settings);
+        this.slideConfig = data;
+
+        return data;
+    }
+
+
+    /**
+     * Set slide data size
+     * set the correct size dependin
+     * on the slide type
+     *
+     * @param { object } data
+     * @param { object } settings
+     * @return { object }
+     */
+    setSize(data, settings) {
+        const defaultWith = (data.type == 'video' ? this.checkSize(settings.videosWidth) : this.checkSize(settings.width));
+        const defaultHeight = this.checkSize(settings.height);
+
+        data.width = (has(data, 'width') && data.width !== '' ? this.checkSize(data.width) : defaultWith);
+        data.height = (has(data, 'height') && data.height !== '' ? this.checkSize(data.height) : defaultHeight);
+
+        return data;
+    }
+
+
+    /**
+     * [checkSize size
+     * check if the passed size has a correct unit
+     *
+     * @param {string} size
+     * @return {string}
+     */
+    checkSize(size) {
+        return (isNumber(size) ? `${size}px` : size);
+    }
+
+
+    /**
+     * Sanitize data attributes value
+     *
+     * @param string val
+     * @return mixed
+     */
+    sanitizeValue(val) {
+        if (val !== 'true' && val !== 'false') {
+            return val;
+        }
+        return (val === 'true');
+    }
+}
---
+++ src/js/core/slide.js
@@ -0,0 +1,237 @@
+/**
+ * Slide
+ * class to hablde slide creation
+ * and config parser
+ */
+
+import ZoomImages from './zoom.js';
+import DragSlides from './drag.js';
+import slideImage from '../slides/image.js';
+import slideVideo from '../slides/video.js';
+import slideInline from '../slides/inline.js';
+import slideIframe from '../slides/iframe.js';
+import SlideConfigParser from './slide-parser.js';
+import { addEvent, addClass, removeClass, hasClass, closest, isMobile, isFunction, createHTML } from '../utils/helpers.js';
+
+export default class Slide {
+    constructor(el, instance) {
+        this.element = el;
+        this.instance = instance;
+    }
+
+    /**
+     * Set slide content
+     *
+     * @param {node} slide
+     * @param {object} data
+     * @param {function} callback
+     */
+    setContent(slide = null, callback = false) {
+        if (hasClass(slide, 'loaded')) {
+            return false
+        }
+
+        const settings = this.instance.settings;
+        const slideConfig = this.slideConfig;
+        const isMobileDevice = isMobile();
+
+        if (isFunction(settings.beforeSlideLoad)) {
+            settings.beforeSlideLoad({
+                index: slideConfig.index,
+                slide: slide,
+                player: false
+            });
+        }
+
+        let type = slideConfig.type;
+        let position = slideConfig.descPosition;
+        let slideMedia = slide.querySelector('.gslide-media');
+        let slideTitle = slide.querySelector('.gslide-title');
+        let slideText = slide.querySelector('.gslide-desc');
+        let slideDesc = slide.querySelector('.gdesc-inner');
+        let finalCallback = callback;
+
+        // used for image accessiblity
+        let titleID = 'gSlideTitle_' + slideConfig.index;
+        let textID = 'gSlideDesc_' + slideConfig.index;
+
+        if (isFunction(settings.afterSlideLoad)) {
+            finalCallback = () => {
+                if (isFunction(callback)) { callback() }
+                settings.afterSlideLoad({
+                    index: slideConfig.index,
+                    slide: slide,
+                    player: this.instance.getSlidePlayerInstance(slideConfig.index)
+                });
+            }
+        }
+
+        if (slideConfig.title == '' && slideConfig.description == '') {
+            if (slideDesc) {
+                slideDesc.parentNode.parentNode.removeChild(slideDesc.parentNode);
+            }
+        } else {
+            if (slideTitle && slideConfig.title !== '') {
+                slideTitle.id = titleID;
+                slideTitle.innerHTML = slideConfig.title;
+            } else {
+                slideTitle.parentNode.removeChild(slideTitle);
+            }
+            if (slideText && slideConfig.description !== '') {
+                slideText.id = textID;
+                if (isMobileDevice && settings.moreLength > 0) {
+                    slideConfig.smallDescription = this.slideShortDesc(slideConfig.description, settings.moreLength, settings.moreText)
+                    slideText.innerHTML = slideConfig.smallDescription;
+                    this.descriptionEvents(slideText, slideConfig);
+                } else {
+                    slideText.innerHTML = slideConfig.description;
+                }
+            } else {
+                slideText.parentNode.removeChild(slideText);
+            }
+            addClass(slideMedia.parentNode, `desc-${position}`);
+            addClass(slideDesc.parentNode, `description-${position}`);
+        }
+
+        addClass(slideMedia, `gslide-${type}`)
+        addClass(slide, 'loaded');
+
+        if (type === 'video') {
+            slideVideo.apply(this.instance, [slide, slideConfig, finalCallback])
+            return
+        }
+
+        if (type === 'external') {
+            slideIframe.apply(this, [slide, slideConfig, finalCallback])
+            return
+        }
+
+        if (type === 'inline') {
+            slideInline.apply(this.instance, [slide, slideConfig, finalCallback])
+            if (slideConfig.draggable) {
+                new DragSlides({
+                    dragEl: slide.querySelector('.gslide-inline'),
+                    toleranceX: settings.dragToleranceX,
+                    toleranceY: settings.dragToleranceY,
+                    slide: slide,
+                    instance: this.instance,
+                });
+            }
+            return
+        }
+
+        if (type === 'image') {
+            slideImage(slide, slideConfig, () => {
+                const img = slide.querySelector('img');
+
+                if (slideConfig.draggable) {
+                    new DragSlides({
+                        dragEl: img,
+                        toleranceX: settings.dragToleranceX,
+                        toleranceY: settings.dragToleranceY,
+                        slide: slide,
+                        instance: this.instance,
+                    });
+                }
+                if (slideConfig.zoomable && img.naturalWidth > img.offsetWidth) {
+                    addClass(img, 'zoomable')
+                    new ZoomImages(img, slide, () => {
+                        this.instance.resize();
+                    });
+                }
+
+                if (isFunction(finalCallback)) {
+                    finalCallback()
+                }
+            })
+            return;
+        }
+
+        if (isFunction(finalCallback)) {
+            finalCallback()
+        }
+    }
+
+
+
+    slideShortDesc(string, n = 50, wordBoundary = false) {
+        let useWordBoundary = wordBoundary
+        string = string.trim()
+        if (string.length <= n) {
+            return string;
+        }
+        let subString = string.substr(0, n - 1);
+        if (!useWordBoundary) {
+            return subString
+        }
+        return subString + '... <a href="#" class="desc-more">' + wordBoundary + '</a>'
+    }
+
+
+
+    descriptionEvents(desc, data) {
+        let moreLink = desc.querySelector('.desc-more')
+        if (!moreLink) {
+            return false
+        }
+
+        addEvent('click', {
+            onElement: moreLink,
+            withCallback: (event, target) => {
+                event.preventDefault();
+                const body = document.body
+
+                let desc = closest(target, '.gslide-desc')
+                if (!desc) {
+                    return false
+                }
+
+                desc.innerHTML = data.description
+                addClass(body, 'gdesc-open')
+
+                let shortEvent = addEvent('click', {
+                    onElement: [body, closest(desc, '.gslide-description')],
+                    withCallback: (event, target) => {
+                        if (event.target.nodeName.toLowerCase() !== 'a') {
+                            removeClass(body, 'gdesc-open')
+                            addClass(body, 'gdesc-closed')
+                            desc.innerHTML = data.smallDescription
+                            this.descriptionEvents(desc, data)
+
+                            setTimeout(() => {
+                                removeClass(body, 'gdesc-closed')
+                            }, 400);
+                            shortEvent.destroy()
+                        }
+                    }
+                })
+            }
+        })
+    }
+
+    /**
+     * Create Slide Node
+     *
+     * @return { node }
+     */
+    create() {
+        return createHTML(this.instance.settings.slideHtml);
+    }
+
+
+    /**
+     * Get slide config
+     * returns each individual slide config
+     * it uses SlideConfigParser
+     * each slide can overwrite a global setting
+     * read more in the SlideConfigParser class
+     *
+     * @return { object }
+     */
+    getConfig() {
+        const parser = new SlideConfigParser();
+        this.slideConfig = parser.parseConfig(this.element, this.instance.settings);
+
+        return this.slideConfig;
+    }
+}
