09172a8a35e07c3884d8d0691f2779631c9fbbc1
biati-digital <mannuel@biati.digital>
2020-07-30 15:04:23 -0500

    Added dragAutoSnap code

--- src/js/core/drag.js
+++ src/js/core/drag.js
@@ -82,6 +82,16 @@ export default class DragSlides {
         this.yOffset = 0;
         this.active = false;
 
+        if (this.doSlideChange) {
+            this.instance.preventOutsideClick = true;
+            this.doSlideChange == 'right' && this.instance.prevSlide();
+            this.doSlideChange == 'left' && this.instance.nextSlide();
+        }
+
+        if (this.doSlideClose) {
+            this.instance.close();
+        }
+
         if (!this.toleranceReached) {
             this.setTranslate(this.dragContainer, 0, 0, true);
         }
@@ -117,6 +127,8 @@ export default class DragSlides {
 
             this.el.isDragging = true;
             this.dragging = true;
+            this.doSlideChange = false;
+            this.doSlideClose = false;
 
             let currentXInt = Math.abs(this.currentX);
             let currentYInt = Math.abs(this.currentY);
@@ -126,23 +138,21 @@ export default class DragSlides {
                 this.yOffset = 0;
                 this.lastDirection = 'x';
                 this.setTranslate(this.dragContainer, this.currentX, 0);
-                this.toleranceReached = false;
 
-                if (currentXInt >= this.toleranceX) {
-                    let dragDir = (this.currentX > 0 ? 'right' : 'left');
+                let doChange = this.shouldChange();
+                if (!this.instance.settings.dragAutoSnap && doChange) {
+                    this.doSlideChange = doChange;
+                }
 
-                    if (
-                        (dragDir == 'left' && this.slide !== this.slide.parentNode.lastChild) ||
-                        (dragDir == 'right' && this.slide !== this.slide.parentNode.firstChild)
-                    ) {
-                        this.toleranceReached = true;
-                        this.active = false;
-                        this.instance.preventOutsideClick = true;
-                        this.dragEnd(null);
-                        dragDir == 'right' && this.instance.prevSlide()
-                        dragDir == 'left' && this.instance.nextSlide()
-                       return;
-                    }
+                if (this.instance.settings.dragAutoSnap && doChange) {
+                    this.instance.preventOutsideClick = true;
+                    this.toleranceReached = true;
+                    this.active = false;
+                    this.instance.preventOutsideClick = true;
+                    this.dragEnd(null);
+                    doChange == 'right' && this.instance.prevSlide()
+                    doChange == 'left' && this.instance.nextSlide()
+                    return;
                 }
             }
 
@@ -152,13 +162,46 @@ export default class DragSlides {
                 this.lastDirection = 'y';
                 this.setTranslate(this.dragContainer, 0, this.currentY);
 
-                if (currentYInt >= this.toleranceY) {
-                    this.instance.close()
+                let doClose = this.shouldClose();
+
+                if (!this.instance.settings.dragAutoSnap && doClose) {
+                    this.doSlideClose = true;
+                }
+                if (this.instance.settings.dragAutoSnap && doClose) {
+                    this.instance.close();
                 }
                 return;
             }
         }
     }
+
+    shouldChange() {
+        let doChange = false;
+        let currentXInt = Math.abs(this.currentX);
+
+        if (currentXInt >= this.toleranceX) {
+            let dragDir = (this.currentX > 0 ? 'right' : 'left');
+
+            if (
+                (dragDir == 'left' && this.slide !== this.slide.parentNode.lastChild) ||
+                (dragDir == 'right' && this.slide !== this.slide.parentNode.firstChild)
+            ) {
+                doChange = dragDir;
+            }
+        }
+        return doChange;
+    }
+
+    shouldClose() {
+        let doClose = false;
+        let currentYInt = Math.abs(this.currentY);
+
+        if (currentYInt >= this.toleranceY) {
+            doClose = true;
+        }
+        return doClose;
+    }
+
     setTranslate(node, xPos, yPos, animated = false) {
         if (animated) {
             node.style.transition = "all .2s ease";
