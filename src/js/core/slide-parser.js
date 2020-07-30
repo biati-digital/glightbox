ab5e13b84e0a19b472ea9945fdcdd8d841d0482c
biati-digital <mannuel@biati.digital>
2020-07-30 15:23:07 -0500

    Added default desciption position

--- src/js/core/slide-parser.js
+++ src/js/core/slide-parser.js
@@ -9,7 +9,7 @@ export default class SlideConfigParser {
             title: '',
             type: '',
             description: '',
-            descPosition: '',
+            descPosition: 'bottom',
             effect: '',
             width: '',
             height: '',
