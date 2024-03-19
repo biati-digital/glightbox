aa96e39dab6565c08789ffafa83a0aeb5cb86c7a
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2024-03-19 11:54:09 +0100

    Fix the CSP problem with .gscrollbar-fixer

--- src/js/glightbox.js
+++ src/js/glightbox.js
@@ -189,12 +189,8 @@ class GlightboxInit {
 
         const scrollBar = window.innerWidth - document.documentElement.clientWidth;
         if (scrollBar > 0) {
-            var styleSheet = document.createElement('style');
-            styleSheet.type = 'text/css';
-            styleSheet.className = 'gcss-styles';
-            styleSheet.innerText = `:root { --gscrollbar-fixer: ${scrollBar}px; }`;
-            document.head.appendChild(styleSheet);
-            _.addClass(body, 'gscrollbar-fixer');
+            document.body.style.marginRight = `${scrollBar}px`;
+            _.addClass(body, 'gscrollbar-fixer'); // backwards compatibility
         }
 
         _.addClass(body, 'glightbox-open');
@@ -1239,6 +1235,7 @@ class GlightboxInit {
             const body = document.body;
             _.removeClass(html, 'glightbox-open');
             _.removeClass(body, 'glightbox-open touching gdesc-open glightbox-touch glightbox-mobile gscrollbar-fixer');
+            document.body.style.marginRight = '';
             this.modal.parentNode.removeChild(this.modal);
 
             this.trigger('close');
@@ -1248,10 +1245,6 @@ class GlightboxInit {
                 this.settings.onClose();
             }
 
-            const styles = document.querySelector('.gcss-styles');
-            if (styles) {
-                styles.parentNode.removeChild(styles);
-            }
             this.lightboxOpen = false;
             this.closing = null;
         });
